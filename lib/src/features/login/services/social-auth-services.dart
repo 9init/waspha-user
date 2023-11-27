b42f4c319ec714247ea079dfcf5870f8c238a367
9init <emovent2e@gmail.com>
2023-11-27 23:17:23 +0200

    add example (invokeRequest)

--- lib/src/features/login/services/social-auth-services.dart
+++ lib/src/features/login/services/social-auth-services.dart
@@ -1,6 +1,11 @@
+import 'package:dio/src/dio_exception.dart';
+import 'package:dio/src/response.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:waspha/src/features/login/model/login/login_data.dart';
 import 'package:waspha/src/features/login/model/social_token_data.dart';
 import 'package:waspha/src/features/login/model/token_type.dart';
+import 'package:waspha/src/shared/networking/networking.dart';
+import 'package:waspha/src/shared/networking/results.dart';
 
 class SocialAuthServices {
   Future<SocialTokenData?> singInWithGoogle() async {
@@ -16,5 +21,20 @@ class SocialAuthServices {
       tokenType: TokenType.google,
       token: gAuth.idToken!,
     );
+  }
+
+  // TODO - Check this request
+  Future<LoginData?> invokeRequest(SocialTokenData tokenData) async {
+    final url = "/social-login";
+    final result = await Networking.post(url, {
+      "token": tokenData.token,
+      "token_type": tokenData.tokenType.name,
+    });
+
+    return switch (result) {
+      Success(value: final v) => LoginData.fromJson(v.data["data"]),
+      Failure(failure: final f) => null,
+      Error(exception: final e) => null,
+    };
   }
 }
