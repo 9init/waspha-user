595cb691d2717d575f295fcd26abf1fec929cca7
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2023-12-02 04:13:44 +0200

    [fix]:added flutter form builder for implement text form filed , make it as a reusable component [fixed keyboard hide content issue] test image picker for fetch photo from camera

--- lib/src/features/custom_need/presentation/item_widget_expanded.dart
+++ lib/src/features/custom_need/presentation/item_widget_expanded.dart
@@ -1,44 +1,47 @@
-import 'package:cached_network_image/cached_network_image.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:waspha/src/widgets/custom_text_form_field/custom_text_form_field.dart';
 
 import '../data/item_data.dart';
-
-class ItemWidgetExpanded extends StatefulHookConsumerWidget {
+// Assuming itemsProvider is a FutureProvider<String>
+class ItemWidgetExpanded extends StatefulHookWidget {
   final Item item;
+
   ItemWidgetExpanded({super.key, required this.item});
 
   @override
-  ConsumerState<ItemWidgetExpanded> createState() => _ItemWidgetExpandedState();
+  State<ItemWidgetExpanded> createState() => _ItemWidgetExpandedState();
 }
 
-class _ItemWidgetExpandedState extends ConsumerState<ItemWidgetExpanded> {
-  static final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
+class _ItemWidgetExpandedState extends State<ItemWidgetExpanded> {
   @override
-  Widget build(BuildContext context) {
+  build(BuildContext context) {
     final counter = useState(1);
-    final TextEditingController _nameController = TextEditingController(
-        text: widget.item.name == null ? "" : widget.item.name);
+    final TextEditingController _nameController =
+        useTextEditingController(text: widget.item.name == null ? "" : widget.item.name);
 
-    final TextEditingController _noteController = TextEditingController(
-        text: widget.item.additional_notes == null
-            ? ""
-            : widget.item.additional_notes);
+    final TextEditingController _noteController = useTextEditingController(
+        text: widget.item.additional_notes == null ? "" : widget.item.additional_notes);
 
-    saveItem() {
-      final itemName = _nameController.text;
-      final itemNotes = _noteController.text;
-      if (itemName.isNotEmpty) {
-        setState(() {
-          widget.item.name = itemName;
-          widget.item.additional_notes = itemNotes;
-        });
+    print("Name: ${widget.item.name}");
+    final pickedImagePath = useState<String?>(null);
+
+    ImagePicker picker = ImagePicker();
+
+    void _pickImage(ImagePicker picker) async {
+      final XFile? image = await picker.pickImage(source: ImageSource.camera);
+
+      if (image != null) {
+        // Update the state with the selected image path
+        pickedImagePath.value = image.path;
+        debugPrint('The Image Is pickedImagePath ${pickedImagePath.value}');
       }
     }
 
-    print("Name: ${widget.item.name}");
 
     return Column(
       children: [
@@ -46,52 +49,49 @@ class _ItemWidgetExpandedState extends ConsumerState<ItemWidgetExpanded> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Align(
-              alignment: Alignment.topCenter,
-              child: CircleAvatar(
-                  radius: 35,
-                  backgroundColor: Colors.grey[200],
-                  backgroundImage:
-                      CachedNetworkImageProvider(widget.item.image ?? ""),
-                  child: widget.item.image == null
-                      ? Icon(
-                          Icons.add,
-                        )
-                      : null),
-            ),
+                alignment: Alignment.topCenter,
+                child: Consumer(
+                  builder: (context, ref, child) {
+                    return CircleAvatar(
+                      radius: 35,
+                      backgroundColor: Colors.grey[200],
+                      backgroundImage: pickedImagePath.value != null ? FileImage(File(pickedImagePath.value??'')) : null,
+                      child: pickedImagePath.value == null
+                          ? IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: () {
+                          _pickImage(picker);
+                        },
+                      )
+                          : null,
+                    );
+                  },
+                )),
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.only(left: 10, top: 0),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        textInputAction: TextInputAction.next,
-                        controller: _nameController,
-                        onChanged: (value) {
-                          _nameController.text = value;
-                          widget.item.name = value;
-                        },
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          labelText: 'Item Summary',
-                        ),
-                      ),
-                      TextFormField(
-                        controller: _noteController,
-                        onChanged: (value) {
-                          _noteController.text = value;
-                          widget.item.additional_notes = value;
-                        },
-                        maxLines: 6,
-                        minLines: 1,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          labelText: 'Add a note',
-                        ),
-                      ),
-                    ],
-                  ),
+                child: Column(
+                  children: [
+                    CustomTextFromField(
+                      textInputAction: TextInputAction.next,
+                      textEditingController: _nameController,
+                      onChanged: (String? value) {
+                        _nameController.text = value ?? '';
+                        widget.item.name = value;
+                      },
+                      labelText: 'Item Summary',
+                    ),
+                    CustomTextFromField(
+                      textEditingController: _noteController,
+                      onChanged: (String? value) {
+                        _noteController.text = value ?? '';
+                        widget.item.additional_notes = value;
+                      },
+                      maxLines: 6,
+                      minLines: 1,
+                      labelText: 'Add a note',
+                    ),
+                  ],
                 ),
               ),
             ),
---
+++ lib/src/widgets/custom_text_form_field/custom_text_form_field.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+class CustomTextFromField extends StatelessWidget {
+  const CustomTextFromField({
+    Key? key,
+    required this.textEditingController,
+    this.onChanged,
+    this.onTap,
+    this.onFieldSubmitted,
+    this.onSaved,
+    this.validator,
+    required this.labelText,
+    this.fieldName,
+    this.maxLines,
+    this.minLines,
+     this.textInputAction,
+  }) : super(key: key);
+  final TextEditingController textEditingController;
+  final Function(String?)? onChanged;
+  final GestureTapCallback? onTap;
+  final ValueChanged<String?>? onFieldSubmitted;
+  final Function(String?)? onSaved;
+  final String? Function(String?)? validator;
+  final String labelText;
+  final String? fieldName;
+  final int? maxLines;
+  final int? minLines;
+  final TextInputAction? textInputAction;
+
+  @override
+  Widget build(BuildContext context) {
+    return FormBuilderTextField(
+      name: fieldName ?? '',
+      textInputAction: textInputAction ?? null,
+      controller: textEditingController,
+      onChanged: onChanged,
+      validator: validator,
+      maxLines: maxLines,
+      minLines: minLines,
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        labelText: labelText,
+      ),
+    );
+  }
+}
