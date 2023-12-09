2e76ddb2a9fc0e00c730a83ec0b11536e623cf58
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2023-12-09 23:36:02 +0200

    [feat]:added a new implementnio for dependency injection for avoid passing values from constructor to another and inject it

---
+++ lib/src/core/di/register_module.dart
@@ -0,0 +1,9 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+@module
+abstract class RegisterModule {
+  @preResolve
+  Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
+}
