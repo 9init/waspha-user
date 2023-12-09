f364311e5cb51ae317aa5147ff0a09c9091df2dd
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2023-12-09 23:45:23 +0200

    initialize dependency injection layer and hive for local data-base

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:waspha/src/core/di/index.dart';
 
 import 'firebase_options.dart';
 import 'src/app.dart';
@@ -10,6 +12,10 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await configureDependencies();
+  await Hive.initFlutter();
+  await Hive.openBox('user_location'); // Open a box to store user location
+
 
   runApp(const ProviderScope(child: MyApp()));
 }
