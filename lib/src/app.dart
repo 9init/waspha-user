ee9a35fe4d75bc885fb55998d29ce1d1d43c51da
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2023-12-09 23:44:03 +0200

    [edit]:edit on size git it from XD design added ref route function to route treee

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'routes/routes.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,WidgetRef ref) {
     return ScreenUtilInit(
-      designSize: Size(1290, 2796),
+      designSize: const Size(375, 812),
       minTextAdapt: true,
       builder: (_, child) {
         return MaterialApp.router(
-          routerConfig: router,
+          routerConfig: ref.read(router),
           debugShowCheckedModeBanner: false,
           title: 'WASPHA',
           theme: ThemeData(
