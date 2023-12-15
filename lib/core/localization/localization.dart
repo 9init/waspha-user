121d21acd8c8c384d0040efab447a783adba56b1
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2023-12-15 13:30:01 +0200

    added localization helper functions

---
+++ lib/core/localization/localization.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:waspha/core/localization/generated/l10n.dart';
+
+
+extension LocalizedStrings on BuildContext {
+  /// Returns single instance of [AppLocalizations]
+  /// Used instead of accessing and importing AppLocalizations at every page
+  AppLocalization get localization => AppLocalization.of(this);
+}
