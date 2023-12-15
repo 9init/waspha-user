ea5286c2fcb71848bbf6930fa3b614769bb2f93f
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2023-12-15 15:58:42 +0200

    added new file to put cache Keys

---
+++ lib/core/const/cahce_keys/cache_keys.dart
@@ -0,0 +1,6 @@
+class CacheKeys{
+  static const String userLocation='user_location';
+  static const String userLatitude = 'user_latitude';
+  static const String userLongitude = 'user_longitude';
+  static const String userAddress = 'user_address';
+}
