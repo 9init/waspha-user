8a9e110042e617ba8de6b221c430ac8bab690def
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2023-12-23 12:38:02 +0200

    add some text style from atmoic design and localization text

--- lib/theme/custom_text_theme.dart
+++ lib/theme/custom_text_theme.dart
@@ -1,66 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:waspha/core/assets_gen/fonts.gen.dart';
+import 'package:waspha/core/const/colors/colors.dart';
+import 'package:waspha/core/const/dimension/dimensions.dart';
 
 class CustomTextTheme {
   static TextTheme textTheme() {
-    const FontWeight medium = FontWeight.w600;
-    const FontWeight regular = FontWeight.w400;
-    const FontWeight normal = FontWeight.w500;
-    const FontWeight normalBold = FontWeight.w700;
-    const FontWeight bold = FontWeight.w800;
     return TextTheme(
       titleLarge: TextStyle(
-        fontSize: ScreenUtil().setSp(26),
-        fontWeight: medium,
+        fontSize: ScreenUtil().setSp(40),
+        fontWeight: AppDimensions.bold,
         fontFamily: MyFontFamily.openSans,
       ),
       titleMedium: TextStyle(
-        fontSize: ScreenUtil().setSp(14),
-        fontWeight: normal,
+        fontSize: ScreenUtil().setSp(20),
+        fontWeight: AppDimensions.bold,
         fontFamily: MyFontFamily.openSans,
       ),
       titleSmall: TextStyle(
-        fontSize: ScreenUtil().setSp(18),
-        fontWeight: regular,
+        fontSize: ScreenUtil().setSp(17),
+        fontWeight: AppDimensions.medium,
         fontFamily: MyFontFamily.openSans,
+        color: WasphaColors.black,
       ),
       displayLarge: TextStyle(
-        fontSize: ScreenUtil().setSp(30),
-        fontWeight: bold,
-        fontFamily: MyFontFamily.openSans,
-      ),
-      displayMedium: TextStyle(
-        fontSize: ScreenUtil().setSp(26),
-        fontWeight: normal,
-        fontFamily: MyFontFamily.openSans,
-      ),
+          fontSize: ScreenUtil().setSp(30),
+          fontWeight: AppDimensions.medium,
+          fontFamily: MyFontFamily.openSans,
+          color: WasphaColors.black),
+      // displayMedium: TextStyle(
+      //   fontSize: ScreenUtil().setSp(26),
+      //   fontWeight: normal,
+      //   fontFamily: MyFontFamily.openSans,
+      // ),
       displaySmall: TextStyle(
-        fontSize: ScreenUtil().setSp(16),
-        fontWeight: normal,
-        fontFamily: MyFontFamily.openSans,
-      ),
-      labelSmall: TextStyle(
-        fontSize: ScreenUtil().setSp(16),
-        fontWeight: regular,
-        fontFamily: MyFontFamily.openSans,
-
-      ),
-      headlineMedium: TextStyle(
-        fontSize: ScreenUtil().setSp(20),
-        fontWeight: normalBold,
-        fontFamily: MyFontFamily.openSans,
-      ),
-      headlineSmall: TextStyle(
-        fontSize: ScreenUtil().setSp(18),
-        fontWeight: medium,
-        fontFamily: MyFontFamily.openSans,
-      ),
-      headlineLarge: TextStyle(
-        fontSize: ScreenUtil().setSp(24),
-        fontWeight: medium,
-        fontFamily: MyFontFamily.openSans,
-      ),
+          fontSize: ScreenUtil().setSp(17),
+          fontWeight: AppDimensions.regular,
+          fontFamily: MyFontFamily.openSans,
+          color: WasphaColors.white),
+      // labelSmall: TextStyle(
+      //   fontSize: ScreenUtil().setSp(16),
+      //   fontWeight: regular,
+      //   fontFamily: MyFontFamily.openSans,
+      //
+      // ),
+      // headlineMedium: TextStyle(
+      //   fontSize: ScreenUtil().setSp(20),
+      //   fontWeight: normalBold,
+      //   fontFamily: MyFontFamily.openSans,
+      // ),
+      // headlineSmall: TextStyle(
+      //   fontSize: ScreenUtil().setSp(18),
+      //   fontWeight: medium,
+      //   fontFamily: MyFontFamily.openSans,
+      // ),
+      // headlineLarge: TextStyle(
+      //   fontSize: ScreenUtil().setSp(24),
+      //   fontWeight: medium,
+      //   fontFamily: MyFontFamily.openSans,
+      // ),
     );
   }
 }
