fc4a7b516d3b5d79a650497fa26820edbb62127a
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2023-12-23 12:39:11 +0200

    [feat]:finish implementation of search on contact list

---
+++ lib/src/features/likes/presentation/screens/contact_list_screen/contact_list.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:waspha/core/localization/localization.dart';
+import 'package:waspha/src/features/likes/presentation/screens/contact_list_screen/widgets/build_contact_list_app_bar.dart';
+
+final getContactProvider = StateProvider<String>((ref) => '');
+final getUserNameContactProvider = StateProvider<String>((ref) => '');
+
+class ContactList extends HookWidget {
+  const ContactList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController searchController = useTextEditingController();
+    final ValueNotifier<bool> isSearching = useState(false);
+    final ValueNotifier<List<Contact>> _contacts = useState([]);
+    final ValueNotifier<bool> _permissionDenied = useState(false);
+    final ValueNotifier<List<Contact>> searchedContact = useState([]);
+
+    Future _fetchContacts() async {
+      if (!await FlutterContacts.requestPermission(readonly: true)) {
+        _permissionDenied.value = true;
+      } else {
+        final contacts = await FlutterContacts.getContacts(
+          withProperties: true,
+          withPhoto: true,
+          withGroups: true,
+          withAccounts: true,
+        );
+        _contacts.value = contacts;
+      }
+    }
+
+    useEffect(() {
+      _fetchContacts();
+      return null;
+    }, []);
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        actions: [
+          BuildAppBarActions(
+            searchController: searchController,
+            isSearching: isSearching,
+          ),
+        ],
+        leading: isSearching.value ? Container() : BackButton(),
+        title: isSearching.value
+            ? BuildContactListAppBar(
+                contactList: _contacts.value,
+                searchController: searchController,
+                searchedContact: searchedContact,
+              )
+            : Text(
+                context.localization.choose_contact,
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+      ),
+      body: Consumer(
+        builder: (context, ref, child) {
+          return ListView.builder(
+            itemCount: isSearching.value
+                ? searchedContact.value.length
+                : _contacts.value.length,
+            itemBuilder: (context, index) {
+              return FutureBuilder(
+                future: FlutterContacts.getContact(isSearching.value
+                    ? searchedContact.value[index].id
+                    : _contacts.value[index].id),
+                builder: (context, snapShot) {
+                  if (snapShot.hasData) {
+                    return ListTile(
+                      onTap: () {
+                        ref.read(getContactProvider.notifier).state =
+                            snapShot.data!.phones.first.number;
+                        ref.read(getUserNameContactProvider.notifier).state =
+                            snapShot.data!.displayName;
+
+                        Navigator.pop(context);
+                      },
+                      title: Text(snapShot.data!.displayName),
+                      subtitle: Text(
+                          '${snapShot.data!.phones.isNotEmpty ? snapShot.data!.phones.first.number : '(none)'}'),
+                      leading: const CircleAvatar(
+                        child: Icon(Icons.person),
+                      ),
+                    );
+                  } else {
+                    Text(context.localization.permission_denied);
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/src/features/likes/presentation/screens/contact_list_screen/widgets/build_contact_list_app_bar.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
+import 'package:waspha/core/const/colors/colors.dart';
+import 'package:waspha/src/widgets/custom_text_form_field/custom_text_form_field.dart';
+
+class BuildContactListAppBar extends HookWidget {
+  const BuildContactListAppBar({
+    Key? key,
+    required this.contactList,
+    required this.searchController,
+    required this.searchedContact,
+  }) : super(key: key);
+  final List<Contact> contactList;
+  final TextEditingController searchController;
+  final ValueNotifier<List<Contact>> searchedContact ;
+
+  @override
+  Widget build(BuildContext context) {
+    void addSearchedCharacterToList(String searchedCharacter) {
+      searchedContact.value = contactList
+          .where((contact) => contact.displayName.startsWith(searchedCharacter))
+          .toList();
+      debugPrint('The Data Get From Search Contact Is ${searchedContact.value}');
+
+    }
+
+    return CustomTextFromField(
+      textEditingController: searchController,
+      onChanged: (String? searchedContact) {
+        addSearchedCharacterToList(searchedContact ?? '');
+      },
+    );
+  }
+}
+
+class BuildAppBarActions extends HookWidget {
+  const BuildAppBarActions( {
+    Key? key,
+    required this.searchController,
+    required this.isSearching,
+  }) : super(key: key);
+  final TextEditingController searchController;
+  final ValueNotifier<bool> isSearching;
+
+  @override
+  Widget build(BuildContext context) {
+    void _clearSearch() {
+      searchController.clear();
+    }
+
+    void _stopSearching() {
+      _clearSearch();
+      isSearching.value = false;
+    }
+
+    void _startSearch() {
+      ModalRoute.of(context)!
+          .addLocalHistoryEntry(LocalHistoryEntry(onRemove: _stopSearching));
+      isSearching.value = true;
+    }
+
+    return IconButton(
+      onPressed: () {
+        if (isSearching.value) {
+          _clearSearch();
+          context.pop();
+        } else {
+          _startSearch();
+        }
+      },
+      icon: Icon(
+        isSearching.value ? Icons.clear : Icons.search,
+        color: WasphaColors.black,
+      ),
+    );
+  }
+}
